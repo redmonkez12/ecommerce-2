662d1f127d48ec5155072d156c1d636ff438fafc
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2025-04-20 09:39:40 +0200

    Add home layout

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,9 @@
+﻿export function Footer() {
+    return (
+        <footer className="flex border-t justify-center font-medium p-6">
+            <div className="flex items-center gap-2">
+                <p>funroad, Inc.</p>
+            </div>
+        </footer>
+    );
+}
--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,11 +1,15 @@
 import { PropsWithChildren } from "react";
 import { Navbar } from "./navbar";
+import {Footer} from "@/app/(home)/footer";
 
 export default function Layout({ children }: PropsWithChildren) {
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar/>
-            {children}
+            <div className="flex-1">
+                {children}
+            </div>
+            <Footer/>
         </div>
     );
 }
